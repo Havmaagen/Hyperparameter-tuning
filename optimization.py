629ce2de52625c23e13db40a68e40455f46a6b17
Havmaagen <98764246+Havmaagen@users.noreply.github.com>
2024-10-25 00:46:07 +0200

    Update optimization.py

--- optimization.py
+++ optimization.py
@@ -23,7 +23,7 @@ parser.add_argument("--k_output_min", type=float, default=0.1)
 parser.add_argument("--k_output_max", type=float, default=0.5)
 
 parser.add_argument("study_name", type=str, default="Hyperparameter optimization")
-parser.add_argument("storage", type=str, default="sqlite:///hyperparameter-optimization.db")
+parser.add_argument("storage", type=str, default="hyperparameter-optimization.db")
 parser.add_argument("n_trials", type=int, default=10)
 
 args = parser.parse_args()
@@ -56,5 +56,6 @@ def objective(trial):
 
 # Perform the optimization
 if __name__ == "__main__":
-    study = optuna.load_study(study_name=args.study_name, storage=args.storage)
+    study = optuna.load_study(study_name=args.study_name,
+                              storage="sqlite:///" + args.storage)
     study.optimize(objective, n_trials=args.n_trials)
